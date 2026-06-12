5e58e22bb3f46ab013ce80cf44f3529acae7c969
georgezyvg <georgeputta355@gmail.com>
2026-06-12 13:12:35 +0100

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -7,7 +7,7 @@ def generate_keys_addresses_and_percentage(start, end):
         for i in range(start, end + 1):
             min_range = 2 ** (i - 1)
             max_range = 2 ** i - 1
-            private_key = random.randint(71, 160)
+            private_key = random.randint(min_range, max_range)
             private_key_hex = hex(private_key)[2:]  # Convert to hexadecimal and remove '0x'
 
             # Convert the hex private key back to integer before encoding to WIF
@@ -26,4 +26,5 @@ def generate_keys_addresses_and_percentage(start, end):
 
             # Write to file: index, private key in hex, percentage, address
             file.write(f"{i}, {private_key_hex}, {address}, {percentage:.6f}%\n")
-(71, 160)
+
+generate_keys_addresses_and_percentage(1, 160)
